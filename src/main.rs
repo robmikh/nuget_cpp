2b175a00918efc75c351cd9e4d7d16d84791570c
robmikh <rob.mikh@outlook.com>
2021-10-23 19:48:55 -0700

    add per-project nuget packing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-extern crate glob;
-#[macro_use]
-extern crate clap;
-
 use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::fs::File;
 use std::env;
 use clap::*;
 
 arg_enum!{
+    #[allow(non_camel_case_types)]
     #[derive(Debug)]
     enum Platform {
         x64,
@@ -73,7 +71,7 @@ fn main() {
     if arg_matches.is_present("dir") {
         let current_dir = arg_matches.value_of("dir").unwrap();
         println!("Using {}...", current_dir);
-        env::set_current_dir(current_dir).expect("failed to set current dir");
+        env::set_current_dir(current_dir).expect("Failed to set the current directory.");
     }
 
     if restore {
@@ -102,6 +100,23 @@ fn nuget_restore() {
     }
 }
 
+fn get_files_with_extension<P: AsRef<Path>>(folder_path: P, ext: &str) -> Option<Vec<PathBuf>> {
+    let folder_path = folder_path.as_ref();
+    let file_paths = std::fs::read_dir(folder_path).ok()?;
+    let mut paths = Vec::new();
+    for entry in file_paths {
+        if let Ok(entry) = entry {
+            let file_path = entry.path();
+            if let Some(file_ext) = file_path.extension() {
+                if file_ext == ext {
+                    paths.push(file_path);
+                }
+            }
+        }
+    }
+    Some(paths)
+}
+
 /*
 msbuild <Solution>.sln /property:Configuration=Release /property:Platform=x64
 */
@@ -120,22 +135,21 @@ fn msbuild_release(plat: &str) {
 }
 
 fn get_local_solution() -> std::path::PathBuf {
-    let mut solution_paths = glob::glob("*.sln")
-                                  .expect("failed to look for solutions")
-                                  .collect::<Vec<_>>();
+    let current_dir = std::env::current_dir().expect("Failed to query current directory.");
+    let mut solution_paths = get_files_with_extension(current_dir, "sln").expect("Failed to search files.");
 
-    if solution_paths.len() <= 0 {
+    if solution_paths.is_empty() {
         panic!("No solution files found!");
     } else if solution_paths.len() > 1 {
         panic!("Too many solution files found!");
     }
 
-    solution_paths.pop().unwrap().unwrap()
+    solution_paths.pop().unwrap()
 }
 
-fn nuget_pack() {
-    let nuspec = get_nugetpkg_nuspec();
-    let version = get_nugetpkg_version();
+fn nuget_pack_directory<P: AsRef<Path>>(nuget_path: P) {
+    let nuspec = get_nugetpkg_nuspec(&nuget_path);
+    let version = get_nugetpkg_version(nuget_path);
 
     let status = Command::new("nuget")
                          .arg("pack")
@@ -148,23 +162,57 @@ fn nuget_pack() {
     }
 }
 
-fn get_nugetpkg_nuspec() -> std::path::PathBuf {
-    let mut nuspec_paths = glob::glob("nuget\\*.nuspec")
-                                .expect("failed to look for solutions")
-                                .collect::<Vec<_>>();
+fn nuget_pack_projects() {
+    // Search for nuget directories in project folders
+    // TODO: Read projects from solution file
+    let current_dir = std::env::current_dir().expect("Failed to query current directory.");
+    let entries = std::fs::read_dir(current_dir).expect("Failed to open current directory.");
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let metadata = entry.metadata().unwrap();
+            if metadata.is_dir() {
+                let nuget_path = {
+                    let mut nuget_path = entry.path();
+                    nuget_path.push("nuget");
+                    nuget_path
+                };
+                if nuget_path.exists() {
+                    nuget_pack_directory(nuget_path);
+                }
+            }
+        }
+    }
+}
 
-    if nuspec_paths.len() <= 0 {
+fn nuget_pack() {
+    let nuget_path = Path::new("nuget");
+    if nuget_path.exists() {
+        nuget_pack_directory(nuget_path);
+    } else {
+        nuget_pack_projects();
+    }
+}
+
+fn get_nugetpkg_nuspec<P: AsRef<Path>>(nuget_path: P) -> std::path::PathBuf {
+    let mut nuspec_paths = get_files_with_extension(nuget_path, "nuspec").expect("Failed to look for solution files.");
+
+    if nuspec_paths.is_empty() {
         panic!("No nuspec files found!");
     } else if nuspec_paths.len() > 1 {
         panic!("Too many nuspec files found!");
     }
 
-    nuspec_paths.pop().unwrap().unwrap()
+    nuspec_paths.pop().unwrap()
 }
 
-fn get_nugetpkg_version() -> String {
-    let mut version_file = File::open("nuget\\VERSION")
-                                .unwrap();
+fn get_nugetpkg_version<P: AsRef<Path>>(nuget_path: P) -> String {
+    let version_path = {
+        let mut version_path = nuget_path.as_ref().to_owned();
+        version_path.push("VERSION");
+        version_path
+    };
+    let mut version_file = File::open(version_path)
+                                .expect("Failed to open VERSION file");
 
     let mut version_string = String::new();
     version_file.read_to_string(&mut version_string).unwrap();
