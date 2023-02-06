4c3a24fc4ecc6c2689a3aa3259b989593742b2cc
robmikh <rob.mikh@outlook.com>
2023-02-05 23:15:00 -0800

    project filtering

---
+++ src/cli.rs
@@ -0,0 +1,75 @@
+use std::{str::FromStr, fmt::Display};
+
+use clap::Parser;
+
+/// A tool that assists in packaging C++/WinRT components for NuGet.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+   /// Sets the current directory when running the tool
+   #[arg(short, long)]
+   pub dir: Option<String>,
+
+   /// Restores project, builds all platforms, and packs [overrides everything]
+   #[arg(short, long)]
+   pub all: bool,
+
+   /// Calls nuget restore
+   #[arg(short, long)]
+   pub restore: bool,
+
+   /// Builds the solution for Release
+   #[arg(short, long)]
+   pub build: Vec<Platform>,
+
+   /// Packs the resulting files. Uses the nuget\ directory
+   #[arg(short, long)]
+   pub pack: bool,
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum Platform {
+    x64,
+    ARM,
+    x86,
+    ARM64,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct ParsePlatformError(&'static str);
+
+impl FromStr for Platform {
+    type Err = ParsePlatformError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "x64" => Ok(Platform::x64),
+            "arm" => Ok(Platform::ARM),
+            "x86" => Ok(Platform::x86),
+            "arm64" => Ok(Platform::ARM64),
+            _ => Err(ParsePlatformError(
+                "Invalid resolution value! Expecting: x86, x64, ARM or ARM64.",
+            )),
+        }
+    }
+}
+
+impl Display for Platform {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let string = match self {
+            Platform::x64 => "x64",
+            Platform::ARM => "ARM",
+            Platform::x86 => "x86",
+            Platform::ARM64 => "ARM64",
+        };
+        write!(f, "{}", string)
+    }
+}
+
+impl Display for ParsePlatformError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl std::error::Error for ParsePlatformError {}
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,88 @@
+mod cli;
+
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::fs::File;
 use std::env;
-use clap::*;
 
-arg_enum!{
-    #[allow(non_camel_case_types)]
-    #[derive(Debug)]
-    enum Platform {
-        x64,
-        ARM,
-        x86,
-        ARM64,
-    }
-}
+use clap::Parser;
+use cli::{Args, Platform};
 
 fn main() {
-    let arg_matches = App::new("nuget_cpp")
-                          .version(crate_version!())
-                          .author("Robert Mikhayelyan <rob.mikh@outlook.com>")
-                          .about("A tool that assists in packaging C++/WinRT components for NuGet.")
-                          .arg(Arg::with_name("dir")
-                                   .help("Sets the current directory when running the tool")
-                                   .short("d")
-                                   .long("dir")
-                                   .takes_value(true))
-                          .arg(Arg::with_name("all")
-                                   .help("Restores project, builds all platforms, and packs [overrides everything]")
-                                   .short("a")
-                                   .long("all"))
-                          .arg(Arg::with_name("restore")
-                                   .help("Calls nuget restore")
-                                   .short("r")
-                                   .long("restore"))
-                          .arg(Arg::with_name("build")
-                                   .help("Builds the solution for Release")
-                                   .short("b")
-                                   .long("build")
-                                   .possible_values(&Platform::variants())
-                                   .multiple(true)
-                                   .takes_value(true))
-                          .arg(Arg::with_name("pack")
-                                   .help("Packs the resulting files. Uses the nuget\\ directory")
-                                   .short("p")
-                                   .long("pack"))
-                          .get_matches();
+    let args = Args::parse();
 
-    let all = arg_matches.is_present("all");
-    let restore = all || arg_matches.is_present("restore");
-    let pack = all || arg_matches.is_present("pack");
+    let all = args.all;
+    let restore = all || args.restore;
+    let pack = all || args.pack;
     
-    let mut build = false;
-    let mut platforms : Vec<&str> = Vec::new();
-    if all {
-        build = true;
-
-        let temp = ["x64", "ARM", "x86", "ARM64"];
-        for platform in temp.iter() {
-            platforms.push(platform);
+    let (build, platforms) = {
+        let mut platforms = Vec::with_capacity(args.build.len());
+        for platform in &args.build {
+            platforms.push(format!("{}", platform));
         }
-    } else if arg_matches.is_present("build") {
-        build = true;
-        let build_values = arg_matches.values_of("build").unwrap();
-        for build_value in build_values {
-            platforms.push(build_value);
+        if all && platforms.is_empty() {
+            let build_platforms = [Platform::x64, Platform::x86, Platform::ARM64, Platform::ARM];
+            for platform in build_platforms {
+                platforms.push(format!("{}", platform));
+            }
         }
-    }
+        (all || !args.build.is_empty(), platforms)
+    };
 
-    if arg_matches.is_present("dir") {
-        let current_dir = arg_matches.value_of("dir").unwrap();
+    if let Some(current_dir) = args.dir.as_ref() {
         println!("Using {}...", current_dir);
         env::set_current_dir(current_dir).expect("Failed to set the current directory.");
     }
 
+    // Find our projects
+    let projects = {
+        let mut projects = Vec::new();
+        for project_dir in get_project_dirs_with_nuget_dirs() {
+            let project_paths = get_files_with_extension(project_dir, "vcxproj").expect("Failed to search files.");
+
+            if project_paths.is_empty() {
+                panic!("No project files found!");
+            } else if project_paths.len() > 1 {
+                panic!("Too many project files found!");
+            }
+
+            projects.push(project_paths.first().unwrap().clone());
+        }
+        projects
+    };
+
     if restore {
-        nuget_restore();
+        println!("Restoring...");
+        for project in &projects {
+            nuget_restore(project);
+        }
     }
-    
+
     if build {
-        for platform in platforms {
-            msbuild_release(platform);
+        println!("Building for these platforms: {:?}", &platforms);
+        for project in &projects {
+            for platform in &platforms {
+                msbuild_release(project, &platform);
+            }
         }
     }
     
     if pack {
+        println!("Packing...");
         nuget_pack();
     }
 }
 
-fn nuget_restore() {
+fn nuget_restore<P: AsRef<Path>>(project_path: P) {
+    let solution = get_local_solution();
+    let solution_dir = solution.parent().unwrap();
+
     let status = Command::new("nuget")
                          .arg("restore")
+                         .arg(project_path.as_ref())
+                         .arg("-SolutionDirectory")
+                         .arg(solution_dir)
                          .status()
                          .expect("process failed to execute");
     
@@ -118,9 +109,33 @@ fn get_files_with_extension<P: AsRef<Path>>(folder_path: P, ext: &str) -> Option
 }
 
 /*
+msbuild <Solution>.sln /t:<Project>.vcxproj /property:Configuration=Release /property:Platform=x64
+*/
+fn msbuild_release<P: AsRef<Path>>(project_path: P, plat: &str) {
+    let solution = get_local_solution();
+
+    let project_path = project_path.as_ref();
+    let project_path_stem = project_path.file_stem().unwrap();
+    let project_path_name = project_path_stem.to_str().unwrap();
+    // TODO: Filter other illegal characters
+    let project_name = project_path_name.replace(".", "_");
+
+    let status = Command::new("msbuild")
+                         .arg(solution)
+                         .arg(format!("/t:{}", project_name))
+                         .arg("/property:Configuration=Release")
+                         .arg(format!("/property:Platform={}", plat))
+                         .status()
+                         .expect("process failed to execute");
+    if !status.success() {
+        panic!("msbuild for {} failed!", plat);
+    }
+}
+
+/*
 msbuild <Solution>.sln /property:Configuration=Release /property:Platform=x64
 */
-fn msbuild_release(plat: &str) {
+fn msbuild_release_solution(plat: &str) {
     let solution = get_local_solution();
 
     let status = Command::new("msbuild")
@@ -162,25 +177,37 @@ fn nuget_pack_directory<P: AsRef<Path>>(nuget_path: P) {
     }
 }
 
-fn nuget_pack_projects() {
+fn get_project_dirs_with_nuget_dirs() -> Vec<PathBuf> {
     // Search for nuget directories in project folders
     // TODO: Read projects from solution file
     let current_dir = std::env::current_dir().expect("Failed to query current directory.");
     let entries = std::fs::read_dir(current_dir).expect("Failed to open current directory.");
+    let mut project_dirs = Vec::new();
     for entry in entries {
         if let Ok(entry) = entry {
             let metadata = entry.metadata().unwrap();
             if metadata.is_dir() {
+                let entry_path = entry.path();
                 let nuget_path = {
-                    let mut nuget_path = entry.path();
+                    let mut nuget_path = entry_path.clone();
                     nuget_path.push("nuget");
                     nuget_path
                 };
                 if nuget_path.exists() {
-                    nuget_pack_directory(nuget_path);
+                    project_dirs.push(entry_path);
                 }
             }
         }
+    }
+    project_dirs
+}
+
+fn nuget_pack_projects() {
+    let project_dirs = get_project_dirs_with_nuget_dirs();
+    for project_dir in project_dirs {
+        let mut nuget_path = project_dir;
+        nuget_path.push("nuget");
+        nuget_pack_directory(nuget_path);
     }
 }
 
